abe65df3d9619df04cd4d9981a50582d31095bf0
Cazuist <sovrus@mail.ru>
2020-10-07 11:58:18 +0300

    Add wss protocol

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import DashboardManager from './DashboardManager';
 
 //const dashboard = new DashboardManager(document.body, 'ws://localhost:7070');
-const dashboard = new DashboardManager(document.body, 'ws://ahj89ws.herokuapp.com');
+const dashboard = new DashboardManager(document.body, 'wss://ahj89ws.herokuapp.com');
 
 dashboard.init();
